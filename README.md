ad8f52112f234d5fae29cc54e2c09d93cbbf0f49
Cameron Geiser <cameron.geiser@gmail.com>
2025-09-05 10:21:45 -0700

    initial commit-original site

---
+++ README.md
@@ -0,0 +1,2 @@
+# eatguacamole.com
+
---
+++ copyright.js
@@ -0,0 +1,7 @@
+<!--
+document.write('<div class="copyright">');
+document.write('Copyright &copy; 2011, all rights reserved<br>');
+document.write('<a  href="http://www.EatGuacamole.com/">Guacamole Enterprises, Ltd.</a><br>');
+document.write('admin@eatguacamole.com');
+document.write('</div>');
+-->
