530a342e3f2ad335ac19dc5b4919bb3b97880ec4
Cameron Geiser <cameron.geiser@gmail.com>
2025-12-17 14:11:04 -0800

    added error page, fixed links

--- copyright.js
+++ copyright.js
@@ -1,6 +1,6 @@
 <!--
 document.write('<div class="copyright">');
-document.write('Copyright &copy; 2011, all rights reserved<br>');
+document.write('Copyright &copy; 2025, all rights reserved<br>');
 document.write('<a  href="http://www.EatGuacamole.com/">Guacamole Enterprises, Ltd.</a><br>');
 document.write('admin@eatguacamole.com');
 document.write('</div>');
