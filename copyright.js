d1253af1c117ab63a02ab1ebdb2f3f7babfb0db9
Cameron Geiser <cameron.geiser@gmail.com>
2026-09-08 22:06:01 -0700

    cleanup

--- copyright.js
+++
@@ -1,6 +0,0 @@
-<!--
-document.write('<div class="copyright">');
-document.write('Copyright &copy; 2026, all rights reserved<br>');
-document.write('<a  href="http://www.EatGuacamole.com/">Guacamole Enterprises, Ltd.</a><br>');
-document.write('</div>');
--->
